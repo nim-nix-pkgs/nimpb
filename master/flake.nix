6161e2e887ed2b96542eb499067b1c7ccd50c61b
hugosenari <hugosenari@gmail.com>
2022-03-27 18:23:33 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimpb-master.ref   = "refs/heads/master";
   inputs.src-nimpb-master.owner = "oswjk";
   inputs.src-nimpb-master.repo  = "nimpb";
+  inputs.src-nimpb-master.dir   = "";
   inputs.src-nimpb-master.type  = "github";
   
   inputs."nimpb_protoc".owner = "nim-nix-pkgs";
